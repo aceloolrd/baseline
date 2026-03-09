cd46f577becf8a342a944a38995c9a5de5acc5a3
Danil Arkhipov <132353384+aceloolrd@users.noreply.github.com>
2026-03-10 00:24:22 +0700

    fix save_top_k=0→3, fix metric name in filename

--- callbacks.py
+++ callbacks.py
@@ -1,33 +1,28 @@
 from pytorch_lightning.callbacks import (
-    ModelCheckpoint,
     EarlyStopping,
     LearningRateMonitor,
-    # DeviceStatsMonitor,
+    ModelCheckpoint,
 )
-from config_loader import train_config 
+
+from config_loader import train_config
 
 callbacks = [
     ModelCheckpoint(
-        dirpath=f"{train_config["DEFAULT_ROOT_DIR"]}/checkpoints/{train_config["EXPERIMENT"]}", 
-        filename="{epoch:02d}_{valid_iou:.4f}_{valid_loss:.4f}", 
-        save_top_k=0,  
-        monitor="valid_loss", 
-        mode="min",  
-        save_last=True, 
-        every_n_epochs=1, 
+        dirpath=f"{train_config['DEFAULT_ROOT_DIR']}/checkpoints/{train_config['EXPERIMENT']}",
+        filename="{epoch:02d}_{valid_dataset_iou:.4f}_{valid_loss:.4f}",
+        save_top_k=3,
+        monitor="valid_loss",
+        mode="min",
+        save_last=True,
+        every_n_epochs=1,
     ),
 
     EarlyStopping(
-        monitor="valid_loss", 
-        min_delta=1e-3,  
-        patience=5,  
-        verbose=False, 
+        monitor="valid_loss",
+        min_delta=1e-3,
+        patience=5,
         mode="min",
     ),
 
     LearningRateMonitor(logging_interval="epoch"),
-    
-    # DeviceStatsMonitor(), 
 ]
-
-
